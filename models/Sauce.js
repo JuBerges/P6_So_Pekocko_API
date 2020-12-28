81cd5b50f27e1f3fde86a2f27d93f62ae71807f4
WadeSpectre <mbergesjulien@gmail.com>
2020-12-28 10:44:55 +0100

    model Sauce modifié (Utilisation de mongooseUniqueVAlid pour name

--- models/Sauce.js
+++ models/Sauce.js
@@ -1,19 +1,20 @@
-const { ObjectID } = require("bson");
 const mongoose = require("mongoose");
+const mongooseUniqueValidator = require("mongoose-unique-validator");
 
 const sauceSchema = mongoose.Schema({
-  id: { type: ObjectID, required: true },
   userId: { type: String, required: true },
-  name: { type: String, required: true },
+  name: { type: String, required: true, unique: true },
   manufacturer: { type: String, required: true },
   description: { type: String, required: true },
   mainPepper: { type: String, required: true },
   imageUrl: { type: String, required: true },
   heat: { type: Number, required: true },
-  likes: { type: Number, required: false },
-  dislikes: { type: Number, required: false },
-  usersLiked: { type: Array, required: false },
-  usersDisliked: { type: Array, required: false },
+  likes: { type: Number, required: true },
+  dislikes: { type: Number, required: true },
+  usersLiked: { type: Array, required: true },
+  usersDisliked: { type: Array, required: true },
 });
+
+sauceSchema.plugin(mongooseUniqueValidator);
 
 module.exports = mongoose.model("Sauce", sauceSchema);
