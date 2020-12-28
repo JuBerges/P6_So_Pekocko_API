9828db16253e1cdd0776b320ba6beec6bc74e57a
WadeSpectre <mbergesjulien@gmail.com>
2020-12-28 16:25:52 +0100

    correction du middleware multer pour suppr image et naming

--- middleware/multer-config.js
+++ middleware/multer-config.js
@@ -2,8 +2,9 @@ const multer = require("multer");
 
 const MIME_TYPES = {
   "image/jpg": "jpg",
-  "image/jpeg": "jpg",
+  "image/jpeg": "jpeg",
   "image/png": "png",
+  "image/gif": "gif",
 };
 
 const storage = multer.diskStorage({
@@ -11,10 +12,19 @@ const storage = multer.diskStorage({
     callback(null, "images");
   },
   filename: (req, file, callback) => {
-    const name = file.originalname.split(" ").join("_");
+    const name = file.originalname.split(".")[0].split(" ").join("_");
     const extension = MIME_TYPES[file.mimetype];
-    callback(null, name + Date.now() + "." + extension);
+    mimeTypeIsValid(extension, req);
+    const finalFilename = name + "_" + Date.now() + "." + extension;
+    req.body.finalFileName = finalFilename;
+    callback(null, finalFilename);
   },
 });
 
-module.exports = multer({ storage }).single("image");
+module.exports = multer({ storage: storage }).single("image");
+
+const mimeTypeIsValid = (ext, req) => {
+  if (ext != "jpg" && ext != "jpeg" && ext != "png" && ext != "gif") {
+    req.body.errorMessage = "Le format de l'image n'est pas valide!";
+  }
+};
