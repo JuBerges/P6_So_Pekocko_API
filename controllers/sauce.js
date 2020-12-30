e1b10d6974466364e8fcbc21664187741ec31cc2
WadeSpectre <mbergesjulien@gmail.com>
2020-12-30 16:06:29 +0100

    efface l'ancienne image quand objet modifié

--- controllers/sauce.js
+++ controllers/sauce.js
@@ -1,6 +1,10 @@
 const Sauce = require("../models/Sauce");
 const fs = require("fs");
-
+/*        
+========>|====================================|<========
+========>|----------CREATE NEW SAUCE----------|<========
+========>|====================================|<========
+*/
 exports.createSauce = (req, res) => {
   const sauceObject = JSON.parse(req.body.sauce);
   delete sauceObject._id;
@@ -16,52 +20,98 @@ exports.createSauce = (req, res) => {
   });
   sauce
     .save()
-    .then(() => res.status(201).json({ message: "Objet enregistré !" }))
+    .then(() =>
+      res.status(201).json({ message: "Votre sauce a bien été enregistrée !" })
+    )
     .catch((error) => res.status(400).json({ error }));
 };
-
+/*        
+========>|====================================|<========
+========>|------------MODIFY SAUCE------------|<========
+========>|====================================|<========
+*/
 exports.modifySauce = (req, res) => {
-  const sauceObject = req.file
-    ? {
+  let sauceObject = {};
+  req.file
+    ? //====> Delete old image if a new image is added
+      (Sauce.findOne({
+        _id: req.params.id,
+      }).then((sauce) => {
+        const filename = sauce.imageUrl.split("/images/")[1];
+        fs.unlinkSync(`images/${filename}`);
+      }),
+      (sauceObject = {
         ...JSON.parse(req.body.sauce),
         imageUrl: `${req.protocol}://${req.get("host")}/images/${
           req.file.filename
         }`,
-      }
-    : { ...req.body };
+      }))
+    : //====> If the update does not contain new image
+      (sauceObject = {
+        ...req.body,
+      });
   Sauce.updateOne(
-    { _id: req.params.id },
-    { ...sauceObject, _id: req.params.id }
+    {
+      _id: req.params.id,
+    },
+    {
+      ...sauceObject,
+      _id: req.params.id,
+    }
   )
-    .then(() => res.status(200).json({ message: "Objet modifié !" }))
-    .catch((error) => res.status(400).json({ error }));
+    .then(() =>
+      res.status(200).json({
+        message: "Sauce modifiée !",
+      })
+    )
+    .catch((error) =>
+      res.status(400).json({
+        error,
+      })
+    );
 };
-
+/*        
+========>|====================================|<========
+========>|------------DELETE SAUCE------------|<========
+========>|====================================|<========
+*/
 exports.deleteSauce = (req, res) => {
   Sauce.findOne({ _id: req.params.id })
     .then((sauce) => {
       const filename = sauce.imageUrl.split("/images/")[1];
       fs.unlink(`images/${filename}`, () => {
         Sauce.deleteOne({ _id: req.params.id })
-          .then(() => res.status(200).json({ message: "Objet supprimé !" }))
+          .then(() => res.status(200).json({ message: "Sauce supprimé !" }))
           .catch((error) => res.status(400).json({ error }));
       });
     })
     .catch((error) => res.status(500).json({ error }));
 };
-
+/*        
+========>|====================================|<========
+========>|-----------GET ONE SAUCE------------|<========
+========>|====================================|<========
+*/
 exports.getOneSauce = (req, res) => {
   Sauce.findOne({ _id: req.params.id })
     .then((sauce) => res.status(200).json(sauce))
     .catch((error) => res.status(404).json({ error }));
 };
-
+/*        
+========>|====================================|<========
+========>|-----------GET ALL SAUCES-----------|<========
+========>|====================================|<========
+*/
 exports.getAllSauces = (req, res) => {
   Sauce.find()
     .then((sauces) => res.status(200).json(sauces))
     .catch((error) => res.status(400).json({ error }));
 };
-
+/*        
+========>|====================================|<========
+========>|-------LIKE OR DISLIKE SAUCE--------|<========
+========>|====================================|<========
+*/
 exports.likingSauce = (req, res) => {
   switch (req.body.like) {
     case 0: //====> Remove like or dislike
