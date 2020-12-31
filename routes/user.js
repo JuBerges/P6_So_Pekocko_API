3e5e30b8bd691b5d19efaac980ffc83a817d1490
WadeSpectre <mbergesjulien@gmail.com>
2020-12-31 09:51:36 +0100

    add: ExpressBrute

--- routes/user.js
+++ routes/user.js
@@ -1,11 +1,18 @@
 const express = require("express");
 const router = express.Router();
 const userCtrl = require("../controllers/user");
+const ExpressBrute = require("express-brute"); //====>Prevent brute force attack
+const store = new ExpressBrute.MemoryStore(); //===> Stores state locally, don't use this in production
+const bruteforce = new ExpressBrute(store);
 
 //====>||SIGN UP||<====\\
 router.post("/signup", userCtrl.signup);
 
 //====>||SIGN IN||<====\\
-router.post("/login", userCtrl.login);
+router.post(
+  "/login",
+  bruteforce.prevent, //====> error 429 if we hit this route too often
+  userCtrl.login
+);
 
 module.exports = router;
